6d21dc64e12eecd7a0653c66b26e7ed4b04e10fa
infaan-hub <salehabdulrahim8587@gmail.com>
2026-03-25 15:04:09 +0300

    Fix49

--- Backend/build.sh
+++ Backend/build.sh
@@ -3,6 +3,5 @@ set -o errexit
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-mkdir -p "${MEDIA_ROOT:-media}"
 python manage.py collectstatic --noinput
 python manage.py migrate
